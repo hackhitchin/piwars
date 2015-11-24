6c969c038dbd968ec83c7633fdbb4e2b02cd1285
David Booth <worlds6440@aol.com>
2015-11-24 20:27:44 +0000

    Converted RC script to class and upgraded launcher to start and stop new RC thread on command.

--- launcher.py
+++ launcher.py
@@ -7,17 +7,23 @@ import time
 import drivetrain
 from wiimote import Wiimote, WiimoteException
 
+import threading
+import rc
+
 class launcher:
     def __init__(self):
         # Need a state set for this launcher.
         self.menu = ["Remote Control"]
         self.menu += ["Three Point Turn"]
         self.menu += ["Straight Line Speed"]
+        self.menu += ["Quit Challenge"]
         self.menu += ["Power Off Pi"]
 
         # default menu item is remote control
         self.menu_state = 0
         self.menu_button_pressed = False
+        self.drive = None
+        self.wiimote = None
         # Current Challenge
         self.challenge = None
 
@@ -26,7 +32,10 @@ class launcher:
         if self.menu[self.menu_state]=="Remote Control":
             # Start the remote control
             logging.info("Entering into Remote Control Mode")
-            self.challenge = None
+            self.challenge = rc.rc(self.drive, self.wiimote)
+            # Create and start a new thread running the remote control script
+            self.challenge_thread = threading.Thread(target=self.challenge.run)
+            self.challenge_thread.start()
         elif self.menu[self.menu_state]=="Three Point Turn":
             # Start the three point turn challenge
             logging.info("Starting Three Point Turn Challenge")
@@ -35,6 +44,17 @@ class launcher:
             # Start the straight line speed challenge
             logging.info("Starting Straight Line Speed Challenge")
             self.challenge = None
+        elif self.menu[self.menu_state]=="Quit Challenge":
+            # Gracefully kill any challenge thread
+            if self.challenge:
+                self.challenge.stop()
+                self.challenge = None
+                self.challenge_thread = None
+                logging.info("Stopping Challenge Thread")
+                # Reset menu item back to top of list
+                self.menu_state = 0
+            else:
+                logging.info("No Challenge Challenge Thread")
         elif self.menu[self.menu_state]=="Power Off Pi":
             # Power off the raspberry pi safely 
             # by sending shutdown command to terminal
@@ -62,35 +82,37 @@ class launcher:
         # Set up logging
         logging.basicConfig(stream=sys.stdout, level=logging.INFO)
         # Initiate the drivetrain
-        drive = drivetrain.DriveTrain(pwm_i2c=0x41)
-        wiimote = None
+        self.drive = drivetrain.DriveTrain(pwm_i2c=0x41)
+        self.wiimote = None
         try:
-            wiimote = Wiimote()
+            self.wiimote = Wiimote()
 
         except WiimoteException:
             logging.error("Could not connect to wiimote. please try again")
 
         # Constantly check wiimote for button presses
-        while wiimote:
-            buttons_state = wiimote.get_buttons()
-            nunchuk_buttons_state = wiimote.get_nunchuk_buttons()
-            joystick_state = wiimote.get_joystick_state()
+        while self.wiimote:
+            buttons_state = self.wiimote.get_buttons()
+            nunchuk_buttons_state = self.wiimote.get_nunchuk_buttons()
+            joystick_state = self.wiimote.get_joystick_state()
 
-            logging.info("joystick_state: {0}".format(joystick_state))
-            logging.info("button state {0}".format(buttons_state))
+#            logging.info("joystick_state: {0}".format(joystick_state))
+#            logging.info("button state {0}".format(buttons_state))
             # Always show current menu item
-            logging.info(self.menu[self.menu_state])
+            logging.info("Menu: " + self.menu[self.menu_state])
 
             # Test if B button is pressed
             if joystick_state is None or (buttons_state & cwiid.BTN_B) or (nunchuk_buttons_state & cwiid.NUNCHUK_BTN_Z):
                 # No nunchuk joystick detected or B or Z button 
                 # pressed, must go into neutral for safety
                 logging.info("Neutral")
-                self.set_neutral(drive, wiimote)
+                self.set_neutral(self.drive, self.wiimote)
             else:
                 # Enable motors
-                self.set_drive(drive, wiimote)
-                if (buttons_state & cwiid.BTN_A) or (buttons_state & cwiid.BTN_UP) or (buttons_state & cwiid.BTN_DOWN):
+                self.set_drive(self.drive, self.wiimote)
+                if ((buttons_state & cwiid.BTN_A)
+                    or (buttons_state & cwiid.BTN_UP) 
+                    or (buttons_state & cwiid.BTN_DOWN)):
                     # Looking for state change only
                     if not self.menu_button_pressed and (buttons_state & cwiid.BTN_A):
                         # User wants to select a menu item
--- rc.py
+++ rc.py
@@ -6,41 +6,46 @@ import time
 import drivetrain
 from wiimote import Wiimote, WiimoteException
 
+class rc:
+    def __init__(self, drive, wiimote):
+        self.killed = False
+        self.drive = drive
+        self.wiimote = wiimote
 
-def run():
-    # Set up logging
-    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-    # Initiate the drivetrain
-    drive = drivetrain.DriveTrain(pwm_i2c=0x41)
-    wiimote = None
-    try:
-        wiimote = Wiimote()
+    def stop(self):
+        """Simple method to stop the RC loop"""
+        self.killed = True
 
-    except WiimoteException:
-        logging.error("Could not connect to wiimote. please try again")
-    while wiimote:
-        buttons_state = wiimote.get_buttons()
-        joystick_state = wiimote.get_joystick_state()
+    def run(self):
+        """Start listening to the wiimote and drive the motors"""
+        # Set up logging
+        logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+        # Initiate the drivetrain
+        #drive = drivetrain.DriveTrain(pwm_i2c=0x41)
+        #wiimote = None
+        #try:
+        #    wiimote = Wiimote()
 
-        logging.info("joystick_state: {0}".format(joystick_state))
-        logging.info("button state {0}".format(buttons_state))
+        #except WiimoteException:
+        #    logging.error("Could not connect to wiimote. please try again")
+        while self.wiimote and not self.killed:
+            buttons_state = self.wiimote.get_buttons()
+            joystick_state = self.wiimote.get_joystick_state()
 
-        # Test if B button is pressed
-        if joystick_state is None or (buttons_state & cwiid.BTN_B):
-            logging.info("B button presed - stopping")
-            drive.set_neutral()
-        else:
+            logging.info("joystick_state: {0}".format(joystick_state))
+            logging.info("button state {0}".format(buttons_state))
+
             # Get the normalised joystick postion as a tuple of
             # (throttle, steering), where values are in the range -1 to 1
             joystick_pos = joystick_state['state']['normalised']
             throttle, steering = joystick_pos
-            drive.mix_channels_and_assign(throttle, steering)
+            self.drive.mix_channels_and_assign(throttle, steering)
 
-        time.sleep(0.05)
+            time.sleep(0.05)
 
-if __name__ == "__main__":
-    try:
-        run()
-    except Exception as e:
-        logging.error("Stopping...")
-        logging.exception(e)
+#if __name__ == "__main__":
+#    try:
+#        run()
+#    except Exception as e:
+#        logging.error("Stopping...")
+#        logging.exception(e)
