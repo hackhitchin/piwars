fcb99957ae31b7a60bc18048e772fa08fdab9155
Rob Berwick <rob.berwick@gmail.com>
2015-12-04 21:12:24 +0000

    line following syntax errors also. copy and paste FTW. sigh.

--- line_following.py
+++ line_following.py
@@ -12,6 +12,7 @@ from numpy import interp, clip
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
+
 class Proximity:
     def __init__(self, drive):
         """ Standard Constructor """
@@ -50,13 +51,13 @@ class Proximity:
         """ Main call to run the three point turn script """
         # Drive forward for a set number of seconds keeping distance equal
         logging.info("forward to turning point")
-        self.move_segment( total_timeout=10.0 )
+        self.move_segment(total_timeout=10.0)
 
         # Final set motors to neutral to stop
         self.drive.set_neutral()
         self.stop()
 
-    def move_segment( self, total_timeout=0 ):
+    def move_segment(self, total_timeout=0):
         logging.info("move_segment called with arguments: {0}".format(locals()))
         # Note Line_sensor=0 if no line sensor exit required
         # calculate timeout times
@@ -74,14 +75,18 @@ class Proximity:
                 voltage = self.adc.read_voltage(distance_sensor)
                 voltage_diff = voltage - self.nominal_voltage
                 steering = interp(
-                        voltage_diff,
-                        [self.min_dist_voltage, self.max_dist_voltage]
-                        [self.left_steering, self.right_steering],
-                    )
+                    voltage_diff,
+                    [self.min_dist_voltage, self.max_dist_voltage]
+                    [self.left_steering, self.right_steering],
                 )
 
             # Had to invert throttle and steering channels to match RC mode
-            logging.info("mixing channels: {0} : {1}".format(throttle, steering))
+            logging.info(
+                "mixing channels: {0} : {1}".format(
+                    throttle,
+                    steering
+                )
+            )
             self.drive.mix_channels_and_assign(steering, throttle)
             time.sleep(0.05)
 
